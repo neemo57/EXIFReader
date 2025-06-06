222fd872bbf3f79efcc65b98210eab84c65b5fd0
witch <lmao@email.com>
2025-06-05 20:22:27 -0600

    Added left and right views in ExifReaderView to make my life easier

--- ExifReaderView.py
+++ ExifReaderView.py
@@ -14,6 +14,14 @@ class ExifReaderView(Frame):
         # Load the background
         self.image_label = None
         self.load_background()
+
+        # Create the left and right views
+        self.left_view = LeftView(self, self.main_controller)
+        self.right_view = RightView(self, self.main_controller)
+
+        # Place them correctly homie
+        self.left_view.place(relx=0.05, rely=0.05, relwidth=0.28, relheight=0.9)
+        self.right_view.place(relx=0.35, rely=0.05, relwidth=0.6, relheight=0.9)
         
     def load_background(self):
         self.image_label = tkinter.Label(self, image=self.main_controller.assets["background"])
